b4c03ff986914ce87a9d45e0ccfabc54dbb6636d
Fededada23 <dadamofederico@gmail.com>
2023-03-10 16:52:30 +0100

    created projectCard e projectMain

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,45 @@
+<script>
+export default{
+    name: "ProjectCard",
+    props:{
+        project: Object,
+        baseUrl: String,
+    }
+}
+</script>
+
+<template>
+    <div class="card m-3">
+        <div class="card-body">
+            <div class="card-img-top">
+                <img :src="project.cover_image != null ? `${baseUrl}/storage/${project.cover_image}` : 'https://picsum.photos/200/300'" class="img-fluid" alt="">
+            </div>
+            <div class="card-title p-1">
+                <h5>{{ project.title }}</h5>
+                <p>
+                    <em v-if="project.type">
+                        <strong>Categoria</strong>: {{project.type.name}}
+                    </em>
+                    <em v-else>
+                        Nessuna Categoria
+                    </em>
+                </p>
+                <p>
+                    <em>
+                        <strong>Technologies</strong>:
+                        <span class="badge bg-primary m-2" v-for="technology in project.technologies">
+                            {{technology.name}}
+                        </span>
+                    </em>
+                </p>
+                <a href="#" class="btn btn-warning">
+                    Leggi l'articolo
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/ProjectMain.vue
@@ -0,0 +1,82 @@
+<script>
+import axios from 'axios';
+import ProjectCard from './ProjectCard.vue';
+export default{
+    name: "ProjectMain",
+    data(){
+        return{
+            projects: [],
+            loading: true,
+            baseUrl: 'http://127.0.0.1:8000',
+            
+            // per il PAGINATE
+            lastPage: null,
+            currentPage: 1,
+        }
+    },
+    components:{
+        ProjectCard,
+    },
+    methods: {
+        getProjects(project_page){
+            this.loading = true;
+            axios.get(`${this.baseUrl}/api/projects`, { params: { page: project_page }}).then((response) => {
+                if(response.data.success) {
+                    this.projects = response.data.projects.data
+                    this.currentPage = response.data.projects.current_page
+                    this.lastPage = response.data.projects.last_page
+                    this.loading = false
+                }
+                else {
+                    
+                }
+            });
+        }
+    },
+    mounted() {
+        this.getProjects(this.currentPage);
+    },
+}
+</script>
+
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="text-center">BoolPress</h2>
+            </div>
+        </div>
+        <div v-if="loading" class="col-12 d-flex justify-content-center">
+            <div class="loader">
+            </div>
+        </div>
+        <div v-else class="col-12 d-flex justify-content-center flex-wrap">
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-4" v-for="project in projects" :key="project.id">
+                    <ProjectCard :project="project" :baseUrl="baseUrl"></ProjectCard>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 d-flex justify-content-center">
+                <nav>
+                    <ul class="pagination">
+                        <li :class="currentPage === 1 ? 'disabled' : 'page-item'">
+                            <button class="page-link" @click="getProjects(currentPage - 1)">Prev</button>
+                        </li>
+                        <li :class="currentPage === i ? 'disabled' : 'page-item'" v-for="item in lastPage">
+                            <button class="page-link" @click="getProjects(item)">{{item}}</button>
+                        </li>
+                        <li :class="currentPage === lastPage ? 'disabled' : 'page-item'">
+                            <button class="page-link" @click="getProjects(currentPage + 1)">Next</button>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+
+</style>
