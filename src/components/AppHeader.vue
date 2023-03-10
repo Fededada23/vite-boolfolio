ccb7add3df5a12a8e53f3381fcebab6408f2c912
Fededada23 <dadamofederico@gmail.com>
2023-03-10 17:27:42 +0100

    import appheader in vue

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,46 @@
+<script>
+export default{
+    name: 'AppHeader',
+    data() {
+        return {
+            menuItems: [
+                {
+                    label: 'Homepage',
+                    routeName: 'homepage',
+                },
+                {
+                    label: 'Blog',
+                    routeName: 'project_list',
+                },
+            ]
+        }
+    },
+}
+</script>
+
+<template>
+    <header>
+        <nav class="navbar navbar-expand-md background-nav">
+            <div class="container">
+                <div class="navbar-brand">
+                    <img src="" class="logo">
+                </div>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item" v-for="(item, index) in menuItems" :key="index">
+                            <router-link :to="{name: item.routeName}" class="nav-link text-white">
+                                {{ item.label }}
+                            </router-link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+    .background-nav{
+        background-color: rgb(27, 27, 161);
+    }
+</style>
