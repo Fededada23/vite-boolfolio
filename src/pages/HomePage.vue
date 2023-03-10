8c26bc03cc95a8fa6e554321b2593312437f351c
Fededada23 <dadamofederico@gmail.com>
2023-03-10 17:23:16 +0100

    created homepage and added router.js

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,8 +1,17 @@
 <script>
+    export default{
+        name: 'Homepage',
+    }
 </script>
 
 <template>
-  
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1 class="text-center">BoolPress</h1>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style lang="scss">
---
+++ src/router.js
@@ -0,0 +1,42 @@
+
+import { createRouter, createWebHistory } from 'vue-router';
+
+import HomePage from './pages/HomePage.vue';
+import ProjectList from './pages/ProjectList.vue';
+import SingleProject from './pages/SingleProject.vue';
+import NotFound from './pages/NotFound.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'homepage',
+            component: HomePage
+        },
+        {
+            path: '/blog',
+            name: 'project_list',
+            component: ProjectList
+        },
+
+
+        {
+
+            path: '/posts/:slug',
+            name: 'single-project',
+            component: SingleProject
+        },
+
+
+        {
+            path: '/*',
+            name: 'not-found',
+            component: NotFound
+        },
+    ]
+})
+
+
+
+export { router };
